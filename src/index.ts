e1cbae32ece0a29270c21f475c21f4ae7bd05a5a
Shob3r <andrewpinter23@gmail.com>
2026-03-11 02:41:10 -0700

    re-format console log to access endpoint easier when developing

--- src/index.ts
+++ src/index.ts
@@ -19,5 +19,5 @@ const app = new Elysia()
     .listen(3000);
 
 console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+    `🖥️ Endpoint started at http://${app.server?.hostname}:${app.server?.port}`,
 );
